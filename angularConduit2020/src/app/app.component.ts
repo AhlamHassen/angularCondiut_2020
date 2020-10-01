af54b812e70495031b1f4a060edc33dd642fe3a8
Ahlam Saleh <ahlamsaleh199700@gmail.com>
2020-10-01 23:57:25 +1000

    Was able to create a user

--- angularConduit2020/src/app/app.component.ts
+++ angularConduit2020/src/app/app.component.ts
@@ -1,7 +1,8 @@
 import { Component } from '@angular/core';
 import { HttpClient, HttpHeaders} from '@angular/common/http';
 import { ArticleEnvelope, Article} from './article';
-import { ArticlesService } from './articles.service'
+import { ArticlesService } from './articles.service';
+import { UserService } from './user.service';
 
 @Component({
   selector: 'app-root',
@@ -12,15 +13,23 @@ export class AppComponent {
   title = 'angularConduit2020';
   public articlesService: ArticlesService;
   
-  constructor(articleService: ArticlesService){
+  constructor(articleService: ArticlesService, public userService:UserService){
      this.articlesService = articleService;
   }
 
   create(){
     this.articlesService.createAnonymous("Ahlam", {
-      title: "Ahlam - Http post requests",
+      title: "Amiiy - Http post requests",
       description: "posting into webservers",
       body: "something"
+    });
+  }
+
+  createUser(){
+    this.userService.createuser({
+      userName: "yenal",
+      email: "yeanl@gmail.com",
+      password: "123yenal456"
     });
   }
 
--- angularConduit2020/src/app/user.service.ts
+++ angularConduit2020/src/app/user.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { UserEnvelope, User} from './user';
+import { UserEnvelope, User, CreateCommand} from './user';
 
 @Injectable({
   providedIn: 'root'
@@ -13,16 +13,13 @@ export class UserService {
     this.httpClient = httpClient;
   }
 
-  createuser(userName: string, email: string, password: string){
-    let request = this.httpClient.post<UserEnvelope>("http://swindev.me/user", {
-      userName: userName,
-      email: email,
-      password: password
-    }as User);
+  createuser(User: User){
+    let request = this.httpClient.post<UserEnvelope>("http://swindev.me/users", {
+     user: User
+    }as CreateCommand);
 
     request.subscribe((response) => {
-      
-      
+      this.user = response.user;
     })
  }
 }
