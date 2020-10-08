5ae03c232add4060cff9557a8a917a039e197d34
Ahlam Saleh <ahlamsaleh199700@gmail.com>
2020-10-08 12:25:45 +1100

    Imported routing and added route paths

--- angularConduit2020/src/app/app.module.ts
+++ angularConduit2020/src/app/app.module.ts
@@ -2,19 +2,30 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http'
 
+import { Routes, RouterModule } from '@angular/router';
+
 import { AppComponent } from './app.component';
 import { ArticleDetailsComponent } from './article-details/article-details.component';
 import { ArticleListComponent } from './article-list/article-list.component';
+import { ArticlesComponent } from './routes/articles/articles.component';
+import { AccountComponent } from './routes/account/account.component';
+
+const routes: Routes = [
+  {path: "Articles", component: ArticlesComponent}
+];
 
 @NgModule({
   declarations: [
     AppComponent,
     ArticleDetailsComponent,
-    ArticleListComponent
+    ArticleListComponent,
+    ArticlesComponent,
+    AccountComponent
   ],
   imports: [
     BrowserModule,
-    HttpClientModule
+    HttpClientModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
